c046e76a89b1d16f505011ee9513977b5243d75b
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-17 04:51:06 +0600

    mongodb connected with server

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 
+app.use(cors());
+app.use(express.json());
+
+// MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yjj5r.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('db connected');
+        const serviceCollection = client.db("cleanCo").collection("services");
+
+        // get all services
+        app.get("/services", async (req, res) => {
+            const services = await serviceCollection.find({}).toArray();
+            res.send(services);
+        })
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
-    res.send("Clean Co Live server is up and running")
+    res.send("Clean Co Live server is up and running");
 })
 
 app.listen(port, () => {
